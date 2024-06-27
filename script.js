c47737253055e446245829412f6d257a9c1efb21
1Mrz838 <murzakarimov555@gmail.com>
2024-06-27 17:40:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ let model;
 
 // Загрузите модель
 async function init() {
-    const modelURL = "URL_К_ВАШЕЙ_МОДЕЛИ/model.json";
-    model = await tf.loadGraphModel(modelURL);
+    try {
+        const modelURL = "URL_К_ВАШЕЙ_МОДЕЛИ/model.json";
+        model = await tf.loadGraphModel(modelURL);
+        console.log("Модель загружена");
+    } catch (error) {
+        console.error("Ошибка загрузки модели:", error);
+    }
 }
 
 // Предсказание
 async function predict(image) {
-    const img = tf.browser.fromPixels(image);
-    const resizedImg = tf.image.resizeBilinear(img, [224, 224]); // Измените размер в соответствии с требованиями вашей модели
-    const batchedImg = resizedImg.expandDims(0);
-    const prediction = await model.predict(batchedImg).data();
-
-    return prediction;
+    try {
+        const img = tf.browser.fromPixels(image);
+        const resizedImg = tf.image.resizeBilinear(img, [224, 224]); // Измените размер в соответствии с требованиями вашей модели
+        const batchedImg = resizedImg.expandDims(0);
+        const prediction = await model.predict(batchedImg).data();
+        return prediction;
+    } catch (error) {
+        console.error("Ошибка предсказания:", error);
+    }
 }
 
 document.getElementById('uploadForm').onsubmit = function(event) {
@@ -22,8 +30,13 @@ document.getElementById('uploadForm').onsubmit = function(event) {
     const imgElement = document.createElement("img");
     imgElement.src = URL.createObjectURL(image);
     imgElement.onload = async function() {
-        const prediction = await predict(imgElement);
-        document.getElementById('result').textContent = JSON.stringify(prediction, null, 2);
+        try {
+            const prediction = await predict(imgElement);
+            document.getElementById('result').textContent = JSON.stringify(prediction, null, 2);
+        } catch (error) {
+            document.getElementById('result').textContent = "Ошибка предсказания";
+            console.error("Ошибка предсказания:", error);
+        }
     };
 };
 
